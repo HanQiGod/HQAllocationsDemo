28cd865ed27af8342ac2a27057bacc96f554096c
HanQiGod <1539841933@qq.com>
2018-11-19 10:31:48 +0800

    Initial Commit

---
+++ HQAllocationsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HQAllocationsDemo
+//
+//  Created by 君凯商联网 on 2018/11/19.
+//  Copyright © 2018 君凯商联网. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HQAllocationsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HQAllocationsDemo
+//
+//  Created by 君凯商联网 on 2018/11/19.
+//  Copyright © 2018 君凯商联网. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
